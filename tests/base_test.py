d058c6b84945cbf08dc96889c86aa4a859c28edb
Ian Armstrong <ianwesleyarmstrong@gmail.com>
2021-07-21 13:04:04 -0500

    Added basic test case for CI/CD

---
+++ tests/base_test.py
@@ -0,0 +1,8 @@
+import unittest
+from slurmflow.dag import DAG
+
+class DAGTest(unittest.TestCase):
+
+    def test_name(self):
+        dag = DAG('test')
+        self.assertEqual(dag.name, 'test')
