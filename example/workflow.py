0b9003ea12cf3d7d2fe574595cd9c322880ec0ff
ianwesleyarmstrong <ianwesleyarmstrong@gmail.com>
2021-08-03 15:30:44 -0500

    fixed flake8 violations in example

--- example/workflow.py
+++ example/workflow.py
@@ -2,16 +2,15 @@ from slurmflow import Job
 from slurmflow import DAG
 
 env = {
-    'BEOCAT_DATA_DIR' : '/bulk/kwjordan',
-    'BEOCAT_SAM_DIR' : '/bulk/kwjordan/sam',
-    'BEOCAT_BAM_DIR' : '/bulk/kwjordan/bam'
+    'BEOCAT_DATA_DIR': '/bulk/${USER}',
+    'BEOCAT_SAM_DIR': '/bulk/${USER}/sam',
+    'BEOCAT_BAM_DIR': '/bulk/kwjordan/bam'
 }
 
 with DAG('simple_slurm_workflow', env=env) as d:
     t1 = Job('Task1', 'task1.sh', dag=d)
     t2 = Job('Task2', 'task2.sh', dag=d)
     t3 = Job('Task3', 'task3.sh', dag=d)
-    
 
     [t1, t2] >> t3
 
